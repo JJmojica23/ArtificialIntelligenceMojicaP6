619f4e55d132a7b28ce425a6397dbcdfeb00cc9a
JJmojica23 <jjmojica2015@gmail.com>
2023-12-05 13:42:19 -0800

    Update and Velocity
    Imported two new scenes with their own scripts. One of them is finished which is the different Update void methods (fixedupdate, lateupdate, etc.).  Imported the Velocity package and started working on it. Not much has been changed apart from moving some folders and stuff.

--- ArtificialInteligenceMojica/Assets/Scripts/Drive.cs
+++ ArtificialInteligenceMojica/Assets/Scripts/Drive.cs
@@ -1,76 +1,11 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-
-// A very simplistic car driving on the x-z plane.
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
     public float speed = 10.0f;
-    public float rotationSpeed = 10.0f;
-    float autoSpeed = 0.1f;
-    bool autoPilot = false;
-
-    public GameObject fuel;
-
-    void Start()
-    {
-
-    }
-
-    void CalculateAngle()
-    {
-        Vector3 tF = this.transform.up;
-        Vector3 fD = fuel.transform.position - this.transform.position;
-
-        float dot = tF.x * fD.x + tF.y * fD.y;
-        float angle = Mathf.Acos(dot / (tF.magnitude * fD.magnitude));
-
-        Debug.Log("Angle: " + angle * Mathf.Rad2Deg);
-        Debug.Log("Unity Angle: " + Vector3.Angle(tF, fD));
-
-        Debug.DrawRay(this.transform.position, tF * 10, Color.green, 2);
-        Debug.DrawRay(this.transform.position, fD, Color.red, 2);
-
-        int clockwise = 1;
-        if (Cross(tF, fD).z < 0)
-        {
-            clockwise = -1;
-        }
-
-        float unityAngle = Vector3.SignedAngle(tF, fD, this.transform.forward);
-
-
-        this.transform.Rotate(0, 0, (angle * clockwise * Mathf.Rad2Deg) * 0.2f);
-    }
-
-    float CalculateDistance()
-    {
-        Vector3 tP = this.transform.position;
-        Vector3 fP = fuel.transform.position;
-
-        float distance = Mathf.Sqrt(Mathf.Pow(tP.x - fP.x, 2) + Mathf.Pow(fP.y - tP.y, 2));
-        float unityDistance = Vector3.Distance(tP, fP);
-        Debug.Log("Distance: " + distance);
-        Debug.Log("Unity Distance: " + unityDistance);
-        return distance;
-    }
-
-    Vector3 Cross(Vector3 v, Vector3 w)
-    {
-        float xMult = v.y * w.z - v.z * w.y;
-        float yMult = v.z * w.x - v.x * w.z;
-        float zMult = v.x * w.y - v.y * w.x;
-
-        Vector3 crossProd = new Vector3(xMult, yMult, zMult);
-        return crossProd;
-    }
-
-    void AutoPilot()
-    {
-        CalculateAngle();
-        this.transform.Translate(this.transform.up * autoSpeed, Space.World);
-    }
+    public float rotationSpeed = 100.0f;
 
     void Update()
     {
@@ -85,27 +20,9 @@ public class Drive : MonoBehaviour
         rotation *= Time.deltaTime;
 
         // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
+        transform.Translate(0, 0, translation);
 
         // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CalculateDistance();
-            CalculateAngle();
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            autoPilot = !autoPilot;
-        }
-        if (autoPilot)
-        {
-            if (CalculateDistance() > 5)
-            {
-                AutoPilot();
-            }
-        }
+        transform.Rotate(0, rotation, 0);
     }
 }
