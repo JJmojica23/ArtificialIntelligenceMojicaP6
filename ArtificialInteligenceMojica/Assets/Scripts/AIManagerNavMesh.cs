38037dc47b9b6491dcb3758483537f2cd2401682
JJmojica23 <jjmojica2015@gmail.com>
2024-04-09 13:19:42 -0700

    Test

---
+++ ArtificialInteligenceMojica/Assets/Scripts/AIManagerNavMesh.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIManagerNavMesh : MonoBehaviour
+{
+    List<NavMeshAgent> agents = new List<NavMeshAgent>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] a = GameObject.FindGameObjectsWithTag("AI");
+        foreach (GameObject go in a)
+        {
+            agents.Add(go.GetComponent<NavMeshAgent>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (NavMeshAgent a in agents) 
+                {
+                    a.GetComponent<AIControllerNavMesh>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
