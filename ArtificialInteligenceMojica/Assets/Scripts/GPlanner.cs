3ea5408698269f584945e4c41fe193c075a22e58
JJmojica23 <jjmojica2015@gmail.com>
2024-04-11 13:47:45 -0700

    GOAP part 2
    Added a bunch of scripts (GAction, GAgent, GPlanner) and worked on them. Still not done with some of them if not all.

---
+++ ArtificialInteligenceMojica/Assets/Scripts/GPlanner.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGOAP
+{
+    public NodeGOAP parent;
+    public float cost;
+    public Dictionary<string, int> state;
+    public GAction action;
+
+    public NodeGOAP(NodeGOAP parent, float cost, Dictionary<string, int> allstates, GAction action)
+    {
+        this.parent = parent;   
+        this.cost = cost;
+        this.state = new Dictionary<string, int>(allstates);
+        this.action = action;
+    }
+}
+
+
+public class GPlanner
+{
+    public Queue<GAction> plan(List<GAction> actions, Dictionary<string, int> goal, WorldStates states)
+    {
+        List<GAction> usableActions = new List<GAction>();
+        foreach (GAction a in actions)
+        {
+            if (a.IsAchievable())
+            {
+                usableActions.Add(a);
+            }
+        }
+
+        List<NodeGOAP> leaves = new List<NodeGOAP>();
+        NodeGOAP start = new NodeGOAP(null, 0, GWorld.Instance.GetWorld().GetStates(), null);
+        bool success = BuildGraph(start, leaves, usableActions, goal);
+
+        if(!success)
+        {
+            Debug.Log("NO PLAN");
+            return null;
+        }
+
+        NodeGOAP cheapest = null;
+        foreach (NodeGOAP leaf in leaves)
+        {
+            if(cheapest == null)
+            {
+                cheapest = leaf;
+            }
+            else
+            {
+                if(leaf.cost < cheapest.cost)
+                {
+                    cheapest = leaf;
+                }
+            }
+        }
+
+        List<GAction> result = new List<GAction>();
+        NodeGOAP n = cheapest;
+        while (n != null)
+        {
+            if (n.action != null)
+            {
+                result.Insert(0, n.action);
+            }
+            n = n.parent;
+        }
+
+        Queue<GAction> queue = new Queue<GAction>();
+        foreach (GAction a in result)
+        {
+            queue.Enqueue(a);
+        }
+
+        Debug.Log("The Plan is: ");
+        foreach (GAction a in queue)
+        {
+            Debug.Log("Q: " + a.actionName);
+        }
+
+        return queue;
+    }
+
+    private bool BuildGraph(NodeGOAP parent, List<NodeGOAP> leaves, List<GAction> usableActions, Dictionary<string, int> goal)
+    {
+        bool foundPath = false;
+        foreach (GAction action in usableActions)
+        {
+            if(action.IsAchieveableGiven(parent.state))
+            {
+                Dictionary<string, int> currentState = new Dictionary<string, int>(parent.state);
+                foreach (KeyValuePair<string, int> eff in action.effects)
+                {
+                    if(!currentState.ContainsKey(eff.Key))
+                    {
+                        currentState.Add(eff.Key, eff.Value);   
+                    }
+                }
+
+                NodeGOAP node = new NodeGOAP(parent, parent.cost + action.cost, currentState, action);
+
+                if (GoalAchieved(goal, currentState))
+                {
+                    leaves.Add(node);
+                    foundPath = true;
+                }
+                else
+                {
+                    List<GAction> subset = ActionSubset(usuableActions, action);
+                    bool found = BuildGraph(node, leaves, subset, goal);
+                    if (found)
+                    {
+                        foundPath = true;
+                    }
+                }
+            }
+        }
+
+        return foundPath;
+    }
+}
